67ecc154fb2b14678c54bf9c1d386fa776fa905a
Thanhle297 <stu725102086@hnue.edu.vn>
2025-04-12 16:51:23 +0700

    ver 2.1.11

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,10 @@
 "use client";
-import 'bootstrap/dist/css/bootstrap.css'; 
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation"; // Thay đổi từ next/router sang next/navigation
 import "../styles/not_found.scss";
+import Link from "next/link";
 
 export default function NotFound() {
   const router = useRouter();
@@ -11,7 +12,7 @@ export default function NotFound() {
   useEffect(() => {
     const redirectTimer = setTimeout(() => {
       router.push("/");
-    }, 3000);
+    }, 10000);
 
     return () => clearTimeout(redirectTimer); // Cleanup function để tránh memory leak
   }, []); // Thêm dependency array trống để useEffect chỉ chạy một lần
@@ -26,11 +27,11 @@ export default function NotFound() {
                 <h1 className="text-center ">404</h1>
               </div>
               <div className="contant_box_404">
-                <h3 className="h2">Look like you're lost</h3>
-                <p>the page you are looking for not avaible!</p>
-                <a href="/" className="link_404">
-                  Go to Home
-                </a>
+                <h3 className="h2">Hình như bạn bị lạc phải không?</h3>
+                <p>trang bạn đang tìm kiếm không có sẵn!</p>
+                <Link href="/" className="link_404">
+                  Trở về trang chủ
+                </Link>
               </div>
             </div>
           </div>
